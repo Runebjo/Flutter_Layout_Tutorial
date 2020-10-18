f5f409eff6937c5af0f9e73a9246cca954aa5b25
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-10-18 21:46:58 +0200

    Finished layout codelab

--- lib/main.dart
+++ lib/main.dart
@@ -16,31 +16,65 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(
-                  Icons.account_circle,
-                  size: 50,
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 38,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.account_circle,
+                    size: 50,
+                  ),
                 ),
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Flutter McFlutter',
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                  Text(
-                    'Experienced App Developer',
-                  ),
-                ],
-              ),
-            ],
-          ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Flutter McFlutter',
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                    Text(
+                      'Experienced App Developer',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('123 Main Street'),
+                Text('(415) 555-0198'),
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  Icons.accessibility,
+                ),
+                Icon(
+                  Icons.timer,
+                ),
+                Icon(
+                  Icons.phone_android,
+                ),
+                Icon(
+                  Icons.phone_iphone,
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
